551320e8d49860e0eb1d0bf762ee5b3ac97dcc97
Uche Blessed <ucheblessed1@gmail.com>
2023-05-25 17:14:09 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $message = $_POST['message'];
 
 $mailheader = "From:".$name."<".$email.">\r\n";
 
-$recipient = "example@gmail.com";
+$recipient = "info@tinapa.ng";
 
 mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
